f740695457125846eced6d1466b0c237b312c6e8
Tom Burger <tvburger@Thomass-MacBook-Air.local>
2019-01-06 21:30:54 +0100

    Updates Specifications methods

--- api/src/main/java/io/codeleaf/config/util/Specifications.java
+++ api/src/main/java/io/codeleaf/config/util/Specifications.java
@@ -4,7 +4,9 @@ import io.codeleaf.config.spec.InvalidSettingException;
 import io.codeleaf.config.spec.SettingNotFoundException;
 import io.codeleaf.config.spec.Specification;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Provides utility methods for specifications.
@@ -15,7 +17,19 @@ import java.util.List;
  */
 public final class Specifications {
 
-    public static int parseInt(List<String> field, Specification specification) throws SettingNotFoundException, InvalidSettingException {
+    public static String parseString(Specification specification, String... field) throws SettingNotFoundException {
+        return parseString(specification, Arrays.asList(field));
+    }
+
+    public static String parseString(Specification specification, List<String> field) throws SettingNotFoundException {
+        return Objects.toString(specification.getValue(field));
+    }
+
+    public static int parseInt(Specification specification, String... field) throws SettingNotFoundException, InvalidSettingException {
+        return parseInt(specification, Arrays.asList(field));
+    }
+
+    public static int parseInt(Specification specification, List<String> field) throws SettingNotFoundException, InvalidSettingException {
         try {
             return Integer.parseInt(specification.getValue(String.class, field));
         } catch (NumberFormatException cause) {
@@ -23,7 +37,11 @@ public final class Specifications {
         }
     }
 
-    public static boolean parseBoolean(List<String> field, Specification specification) throws SettingNotFoundException, InvalidSettingException {
+    public static boolean parseBoolean(Specification specification, String... field) throws SettingNotFoundException, InvalidSettingException {
+        return parseBoolean(specification, Arrays.asList(field));
+    }
+
+    public static boolean parseBoolean(Specification specification, List<String> field) throws SettingNotFoundException, InvalidSettingException {
         try {
             boolean bool;
             String value = specification.getValue(String.class, field);
